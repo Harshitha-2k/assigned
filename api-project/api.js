a6a67c120c7f3cba5674fa5d80d227efa7a5e8d7
Harshitha-2k <reddyharshitha2k@gmail.com>
2022-11-16 19:40:01 +0530

    visiting card completed

--- api-project/api.js
+++ api-project/api.js
@@ -2,7 +2,7 @@ fetch('https://jsonplaceholder.typicode.com/posts?userId=1')
   .then((response) => response.json())
   .then((data) => {
     console.log(data)
-    let datas = data.filter((id)=>{return id%2===0});
+    let datas = "";
     data.map((items) => {
     datas = datas + `<p>${items.id}</p>
     <p>${items.title}</p>`
